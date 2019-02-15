35b2842ce60d805f4e95538e289569fd7cb21e32
Albert <alhol@dtu.dk>
2019-02-15 09:45:13 +0100

    Insert Raw Data from Notification: - Implemented insert method on database.py to insert a new measure - When notification is received app decrypts the rawdata and inserts it's value to the db

--- iotApp/database.py
+++ iotApp/database.py
@@ -3,14 +3,24 @@ from flask import Flask, Blueprint
 
 db = Blueprint('Influxdb', __name__)
 
-
 # client = InfluxDBClient(host='mydomain.com', port=8086, username='myuser', password='mypass' ssl=True, verify_ssl=True)
-client = InfluxDBClient(host='localhost', port=8086)
-
+client = InfluxDBClient(host='localhost', port=8086, database='waterflow')
 
 @db.route('/database_status')
 def check_database_status():
     return getMeasures()
+
+
+def insertPoint(tags, measures, timestamp):
+    json_body = [
+        {
+            "measurement": "flow",
+            "tags": tags,
+            "time": timestamp,
+            "fields": measures
+        }
+    ]
+    client.write_points(json_body)
 
 
 def getMeasures():
@@ -18,5 +28,5 @@ def getMeasures():
     points = results.get_points()
     measurements = ""
     for point in points:
-        measurements += "%s, %s, %i \n" % (point['time'], point['devid'], point["value"])
+        measurements += "%s, %s, %i \n" % (point['time'], point['dev_id'], point["value"])
     return measurements
