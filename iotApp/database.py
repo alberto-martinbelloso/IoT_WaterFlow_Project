233369877d47322883a9999c531da17e1034b488
Albert <alhol@dtu.dk>
2019-02-18 10:50:35 +0100

    Develop: - Added envioronment controll to distinguish if connection should be to localhost influxdb or deployed instance

--- iotApp/database.py
+++ iotApp/database.py
@@ -1,10 +1,20 @@
 from influxdb import InfluxDBClient
 from flask import Flask, Blueprint
+import os
 
 db = Blueprint('Influxdb', __name__)
+host = 'localhost'
+
+try:
+    if os.environ["DEPLOY"]:
+        host = os.environ["DB_HOST"]
+except Exception as e:
+    print("Running on develop environment")
+
 
 # client = InfluxDBClient(host='mydomain.com', port=8086, username='myuser', password='mypass' ssl=True, verify_ssl=True)
-client = InfluxDBClient(host='localhost', port=8086, database='waterflow')
+client = InfluxDBClient(host=host, port=8086, database='waterflow')
+
 
 @db.route('/database_status')
 def check_database_status():
